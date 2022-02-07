63acf9a1e9fb9e6903065e75a790238dec5e20cd
amir.yazidi1@esprit.tn <60586399+amiryazidi@users.noreply.github.com>
2022-02-07 12:17:57 +0100

    List user

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../core/model/user";
 
 @Component({
   selector: 'app-user-list',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-
+  listUser: User[];
   constructor() { }
 
   ngOnInit(): void {
+    this.listUser=[
+      {id: 1, username: 'Ahmed', name: 'Ahmed ben foulen', email: 'ahmed@gmail.com',role:'admin'},
+      {id: 2, username: 'Ahmed', name: 'Ahmed ben foulen', email: 'ahmed@gmail.com',role:'simple user'},
+      {id: 3, username: 'Ahmed', name: 'Ahmed ben foulen', email: 'ahmed@gmail.com',role:'admin'},
+      {id: 4, username: 'Ahmed', name: 'Ahmed ben foulen', email: 'ahmed@gmail.com',role:'admin'},
+    ];
   }
-
+  delete(i:number):void{
+    this.listUser.splice(i,1);
+  }
 }
