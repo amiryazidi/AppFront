a131bf7d5e69079f77218a05883b046e34fbe44e
amir.yazidi1@esprit.tn <60586399+amiryazidi@users.noreply.github.com>
2022-02-14 11:14:34 +0100

    input Product - best Seller

--- src/app/best-seller/best-seller.component.ts
+++ src/app/best-seller/best-seller.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../core/model/product";
 
 @Component({
   selector: 'app-best-seller',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./best-seller.component.css']
 })
 export class BestSellerComponent implements OnInit {
-
+  list: Product[];
   constructor() { }
 
   ngOnInit(): void {
+    this.list = [
+      {id: 1, name: 'T-shirt 1',description: 'Lorem ing elit', price: 15, nbrLike: 0, quantity: 10, picture: 'assets/images/t_shirt1.jpg'},
+    ]
   }
 
 }
