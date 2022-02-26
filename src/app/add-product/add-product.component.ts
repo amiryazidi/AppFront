a2375f5ce1e3fa07d3071f71027f5ff18dee8f8f
amir.yazidi1@esprit.tn <60586399+amiryazidi@users.noreply.github.com>
2022-02-26 22:54:57 +0100

    Form Product

---
+++ src/app/add-product/add-product.component.ts
@@ -0,0 +1,21 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Product} from "../core/model/product";
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+export class AddProductComponent implements OnInit {
+  product: Product;
+  @Output () eventAddProduct = new EventEmitter<Product> ();
+  constructor() { }
+
+  ngOnInit(): void {
+    this.product = new Product();
+  }
+  save(){
+    this.product.nbrLike=0;
+    this.eventAddProduct.emit(this.product);
+  }
+}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,10 +10,12 @@ export class ProductListComponent implements OnInit {
   title: string;
   priceMax : number;
   listProduct: Product[];
+  show:boolean;
   constructor() { }
 
   ngOnInit(): void {
     this.title= "MyApp Store";
+    this.show=false;
     this.listProduct = [
       {id: 1, name: 'T-shirt 1',description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 15, nbrLike: 0, quantity: 10, picture: 'assets/images/t_shirt1.jpg'},
       {id: 2, name: 'T-shirt 2',description: 'Ad corporis culpa eligendi, esse fugit id illo ipsam iste iusto ', price: 65, nbrLike: 0, quantity: 0, picture: 'assets/images/t_shirt1.jpg'},
@@ -26,5 +28,13 @@ export class ProductListComponent implements OnInit {
     if(i!=-1){
       this.listProduct[i].nbrLike++;
     }
+  }
+  showForm(): void{
+    this.show = true;
+  }
+
+  saveProduct(p:Product){
+    this.listProduct.push(p);
+    this.show=false;
   }
 }
